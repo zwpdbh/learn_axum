9435b91568447663b027e8cb427ae95b12be1815
zhaowei <zhaowei@microsoft.com>
2024-10-11 10:21:10 +0800

    quick test setup complete

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Need `cargo install cargo-watch`
+run_server:
+	cargo watch -q -c -w src/ -x run 
+
+run_client:
+	cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 #![allow(unused)]
 
+use std::net::SocketAddr;
+
+use axum::{
+    response::Html,
+    routing::{get, Route},
+    Router,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let routers_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong> World </strong>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> Listening on {addr}\n");
+    axum_server::bind(addr)
+        .serve(routers_hello.into_make_service())
+        .await
+        .unwrap();
 }
