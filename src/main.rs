5e10dea1a87188061017f5bfb04534dc3b5b1e28
zhaowei <zhaowei@microsoft.com>
2024-10-11 10:28:17 +0800

    > pass parameter to route

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,14 @@
 use std::net::SocketAddr;
 
 use axum::{
-    response::Html,
+    response::{Html, IntoResponse},
     routing::{get, Route},
     Router,
 };
 
 #[tokio::main]
 async fn main() {
-    let routers_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong> World </strong>") }),
-    );
+    let routers_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> Listening on {addr}\n");
@@ -21,4 +18,9 @@ async fn main() {
         .serve(routers_hello.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html("Hello <strong> World </strong>")
 }
