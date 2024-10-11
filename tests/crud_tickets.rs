026665d7d3e25ec7c7270a81386a4b85b2d1cf17
zhaowei <zhaowei@microsoft.com>
2024-10-11 16:52:52 +0800

    < crud complete > ensure user login to do create and delete

--- tests/crud_tickets.rs
+++ tests/crud_tickets.rs
@@ -24,3 +24,36 @@ async fn create_ticket() -> Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn list_tickets() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd": "welcome"
+        }),
+    );
+    req_login.await?.print().await?;
+
+    hc.do_get("/api/tickets").await?.print().await?;
+    Ok(())
+}
+
+#[tokio::test]
+async fn delete_ticket() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd": "welcome"
+        }),
+    );
+    req_login.await?.print().await?;
+
+    hc.do_delete("/api/tickets/0").await?.print().await?;
+
+    Ok(())
+}
