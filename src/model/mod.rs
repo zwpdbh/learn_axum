84abb6bf2b28c2acc55e6337ff600ff887521f84
zhaowei <zhaowei@microsoft.com>
2024-10-15 14:51:03 +0800

    < design

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,21 @@
+//! Model Layer
+//!
+//! Design:
+//!
+//! - The Model layer normalizes the application's data type
+//!   structures and access.
+//! - All application code data access must go through the Model layer.
+//! - The `ModelManager` holds the internal states/resources
+//!   needed by ModelControllers to access data.
+//!   (e.g., db_pool, S3 client, redis client).
+//! - Model Controllers (e.g., `ConvBmc`, `AgentBmc`) implement
+//!   CRUD and other data access methods on a given "entity"
+//!   (e.g., `Conv`, `Agent`).
+//!   (`Bmc` is short for Backend Model Controller).
+//! - In frameworks like Axum, Tauri, `ModelManager` are typically used as App State.
+//! - ModelManager are designed to be passed as an argument
+//!   to all Model Controllers functions.
+
 // region:    --- Modules
 
 mod error;
