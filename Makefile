1139bd0f9b3553ad556d36110d9fff5b14804584
zhaowei <zhaowei@microsoft.com>
2024-10-15 13:52:07 +0800

    > use docker-compose

--- Makefile
+++ Makefile
@@ -16,3 +16,16 @@ run_client:
 
 # test_auth_middleware_failed:
 # 	cargo watch -q -c -w tests/ -x "test --test auth_middleware_failed -- --nocapture"
+
+
+# Starting the DB
+# Start postgresql server docker image:
+run_db_dev: 
+	docker run --rm --name pg -p 5432:5432 -e POSTGRES_PASSWORD=welcome postgres:16
+run_docker_file_for_dev:
+	
+
+# (optional) To have a psql terminal on pg. 
+# In another terminal (tab) run psql:
+run_psql_terminal:
+	docker exec -it -u postgres pg psql
--- sql/dev_inital/00-recreate-db.sql
+++ sql/dev_inital/00-recreate-db.sql
@@ -1,7 +1,16 @@
 -- DEV ONLY - Brute Force DROP DB (for local dev and unit test)
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE username = 'app_user' OR datname = 'app_db';
+SELECT
+  pg_terminate_backend(pid)
+FROM
+  pg_stat_activity
+WHERE
+  usename = 'app_user'
+  OR datname = 'app_db';
+
 DROP DATABASE IF EXISTS app_db;
+
 DROP USER IF EXISTS app_user;
 
 CREATE USER app_user PASSWORD 'dev_only_pwd';
+
 CREATE DATABASE app_db owner app_user ENCODING = 'UTF-8';
